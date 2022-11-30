08d077cc680eea74f24a3ea26f9d917d300f736a
szymon cieszynski <szymon.cieszynski.programista@gmail.com>
2022-11-30 17:45:09 +0100

    change page after logg in and logout action

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -27,6 +27,8 @@ class Login extends \Core\Controller
        // $remember_me = isset($_POST['remember_me']);
 
         if ($user) {
+            session_regenerate_id(true);
+            $_SESSION['user_name'] = $user->username;
             $this->redirect('/');
             /*Auth::login($user, $remember_me);
             Flash::addMessage('Login successful');
@@ -38,6 +40,31 @@ class Login extends \Core\Controller
                 'remember_me' => $remember_me*/
             ]);
         }
+    }
+
+    //Log out - zniszczenie sesji
+    public function destroyAction()
+    {
+        //Auth::logout();
+
+        //$this->redirect('/login/show-logout-message');
+
+        $_SESSION = [];
+
+        // If it's desired to kill the session, also delete the session cookie.
+        // Note: This will destroy the session, and not just the session data!
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+            );
+        }
+
+        // Finally, destroy the session.
+        session_destroy();
+
+        $this->redirect('/');
     }
 
 

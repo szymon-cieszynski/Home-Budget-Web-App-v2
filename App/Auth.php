4bd203ed2b98414e57babc9935faf8f14227fd67
szymon cieszynski <szymon.cieszynski.programista@gmail.com>
2022-12-01 19:14:06 +0100

    add RemmemberedLogin class and automatically log in

--- App/Auth.php
+++ App/Auth.php
@@ -65,7 +65,7 @@ class Auth
         // Finally destroy the session
         session_destroy();
 
-        //static::forgetLogin();
+        static::forgetLogin();
     }
 
 
@@ -92,9 +92,59 @@ class Auth
     /*Get current user */
     public static function getUser()
     {
-        if (isset($_SESSION['user_id']))
-        {
-            return User::findById($_SESSION['user_id']);
+        if (isset($_SESSION['user_id'])) {
+
+            return User::findByID($_SESSION['user_id']);
+        } else {
+
+            return static::loginFromRememberCookie();
         }
     } 
+
+        /**
+     * Login the user from a remembered login cookie
+     *
+     * @return mixed The user model if login cookie found; null otherwise
+     */
+    protected static function loginFromRememberCookie()
+    {
+        $cookie = $_COOKIE['remember_me'] ?? false;
+
+        if ($cookie) {
+
+            $remembered_login = RememberedLogin::findByToken($cookie);
+
+            if ($remembered_login && !$remembered_login->hasExpired()) {
+
+                $user = $remembered_login->getUser();
+
+                static::login($user, false);
+
+                return $user;
+            }
+        }
+    }
+
+    
+    /**
+     * Forget the remembered login, if present
+     *
+     * @return void
+     */
+    protected static function forgetLogin()
+    {
+        $cookie = $_COOKIE['remember_me'] ?? false;
+
+        if ($cookie) {
+
+            $remembered_login = RememberedLogin::findByToken($cookie);
+
+            if ($remembered_login) {
+
+                $remembered_login->delete();
+            }
+
+            setcookie('remember_me', '', time() - 3600);  // set to expire in the past
+        }
+    }
 }

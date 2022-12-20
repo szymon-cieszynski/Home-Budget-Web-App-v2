f34cdd5f9c74eb91366c75cb75e9c0bd76d450cb
szymon cieszynski <szymon.cieszynski.programista@gmail.com>
2022-12-20 16:29:16 +0100

    add Profile class and views

---
+++ App/Controllers/Profile.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controllers;
+
+use \App\Auth;
+
+use \Core\View;
+use \App\Flash;
+
+/**
+ * Profile controller
+ */
+class Profile extends Authenticated
+{
+    /**
+     * Before filter - called before each action method
+     *
+     * @return void
+     */
+    protected function before()
+    {
+        parent::before(); //to wywoła metodę która wymaga zalogowania do strony jeśli chcemy mieć dostęp do edycji danych..!!
+
+        $this->user = Auth::getUser(); //unikamy redundacji kodu
+    }
+
+    /**
+     * Show the profile
+     *
+     * @return void
+     */
+    public function showAction()
+    {
+        View::renderTemplate('Profile/show.html', [
+            'user' => $this->user
+        ]);
+    }
+
+    /**
+     * Show the form for editing the profile
+     *
+     * @return void
+     */
+    public function editAction()
+    {
+        View::renderTemplate('Profile/edit.html', [
+            'user' => $this->user
+        ]);
+    }
+
+    /**
+     * Update the profile
+     *
+     * @return void
+     */
+    public function updateAction()
+    {
+        if ($this->user->updateProfile($_POST)) {
+
+            Flash::addMessage('Changes saved');
+
+            $this->redirect('/profile/show');
+        } else {
+
+            View::renderTemplate('Profile/edit.html', [
+                'user' => $this->user
+            ]);
+        }
+    }
+}

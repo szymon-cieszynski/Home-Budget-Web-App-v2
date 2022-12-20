c98d3ff6831172ffb0174f890736cac7bf213f66
szymon cieszynski <szymon.cieszynski.programista@gmail.com>
2022-12-20 19:36:07 +0100

    some improvements

--- App/Controllers/Balance.php
+++ App/Controllers/Balance.php
@@ -46,6 +46,9 @@ class Balance extends \Core\Controller
 
             $sumIncomes = Incomes::getSumOfIncomes($user_incomes);
             $sumExpenses = Expenses::getSumOfExpenses($user_expenses);
+
+            //var_dump($user_incomes);
+            //var_dump($user_expenses);
             
             //var_dump($dataPointsIncomes);
 
